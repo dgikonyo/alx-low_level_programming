1911b8159d6871b7c886b8e46627f2b97f218696
root <denniskimani05@gmail.com>
2022-03-15 02:13:23 -0700

    Task 5- 1.2

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-/*
+
+/**
  * main - entry point
  *
  * Description: Prints "with proper grammar, but the outcome is a piece of art"
  *
  * Return : 0 for success
  */
-int maini(void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
