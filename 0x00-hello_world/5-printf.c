342bcd4b1a3a346ab589b7e3352ec6a47dffa927
root <denniskimani05@gmail.com>
2022-03-15 02:06:35 -0700

    k:q :q Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,13 @@
 /*
  * main - entry point
  *
- * Description: Prints "with proper grammar, but the outcome is a piece of art"
+ * Description: Prints "with proper grammar, but the outcome is a piece 
+ * of art"
  *
  * Return : 0 for success
  */
-int main (void)
+int maini(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
