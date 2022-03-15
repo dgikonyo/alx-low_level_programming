461a56ada902d3de531bdb2a2586a4f38ba08cab
root <denniskimani05@gmail.com>
2022-03-15 02:10:58 -0700

    Task 5- 1.2

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,8 +2,7 @@
 /*
  * main - entry point
  *
- * Description: Prints "with proper grammar, but the outcome is a piece 
- * of art"
+ * Description: Prints "with proper grammar, but the outcome is a piece of art"
  *
  * Return : 0 for success
  */
