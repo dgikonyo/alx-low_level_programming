290620da2bd4b4dd1bbef67480d4cdad14340fb8
dgikonyo <denniskimani05@gmail.com>
2022-03-17 11:07:00 +0300

    task 2 edits

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,7 @@
 #include "main.h"
-
-/*
- * print_alphabet_x10 - Entry point
- *
- * Return: void
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ * Return:void
  */
 
 void print_alphabet_x10(void)
