7fc537c850e12de37e5646f1389c302c2b4dbc7c
dgikonyo <denniskimani05@gmail.com>
2022-03-17 15:54:11 +0300

    task 12 edits

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,6 +17,7 @@ void print_to_98(int n)
 		while (n > 98)
 		{
 			printf("%d, ",n);
+			n--;
 		}
 	}
 	else if (n < 98)
@@ -24,6 +25,7 @@ void print_to_98(int n)
 		while (n < 98)
 		{
 			printf("%d, ",n);
+			n++;
 		}
 	}
 	printf("98\n");
