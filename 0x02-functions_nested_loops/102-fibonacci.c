3c248db82ce99d43c37ac68633cade78d309f82e
dgikonyo <denniskimani05@gmail.com>
2022-03-18 15:17:48 +0300

    task 13 -edits

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,9 +6,9 @@ int main(void)
 	int j;
 	int f[51];
 
-	for (i = 1; i <= len(f); i++)
+	for (i = 1; i <= sizeof (f); i++)
 	{
-		for (j = 2; j < len(f); j++)
+		for (j = 2; j < sizeof (f); j++)
 		{
 			if ( i == 1 || j == 2){
 				printf("%d\n", i);
