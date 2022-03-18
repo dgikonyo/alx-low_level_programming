447f3e6fe6dd0162f6cfd82091d1f6a802aac468
dgikonyo <denniskimani05@gmail.com>
2022-03-18 15:49:39 +0300

    task 13 -edits

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 
-int main(void)
-{	
-	int i;
+int fibo(int n)
+{
 	int fib[n + 2];
 	fib[0] = 1;
 	fib[1] = 2;
 
-	for (i = 3; i < 50; i++)
+	for (int i = 3;i <= n; i++)
 	{
 		fib[i] = fib[i - 1] + fib[i - 2];
-		
-		printf("%u\n", fib[i]);
 	}
+	return fib[n];
+}
+
+int main(void)
+{	
+	int n = 50;
+
+	printf("%d",fibo(n));
 	return (0);
 }
