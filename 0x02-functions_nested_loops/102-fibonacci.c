4606c1c58b9e8319e7d12a3f6c388d7115b4119d
dgikonyo <denniskimani05@gmail.com>
2022-03-18 15:16:13 +0300

    task 13 -edits

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,15 +6,11 @@ int main(void)
 	int j;
 	int f[51];
 
-	for (i = 1; i <= 50; i++)
+	for (i = 1; i <= len(f); i++)
 	{
-		if (i == 1 || i == 2)
+		for (j = 2; j < len(f); j++)
 		{
-			printf("%d\n", i);
-		}
-		for (j = i - 2; j < 50; j++)
-		{
-			if ( i == 1 || i == 2){
+			if ( i == 1 || j == 2){
 				printf("%d\n", i);
 			}
 			f[i] = f[i - 1] + f[j];
