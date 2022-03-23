13dac6ae720fff3fb76f9d1a691e5f7363f8de72
dgikonyo <denniskimani05@gmail.com>
2022-03-23 09:59:20 +0300

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,7 +22,7 @@ void print_times_table(int n)
 				{
 					_putchar(product + '0');
 				}
-				else if (product < 10 && column!=0 )
+				else if (product < 10 && column != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
@@ -43,8 +43,8 @@ void print_times_table(int n)
 					_putchar(',');
 					_putchar(' ');
 					_putchar(product / 100 + '0');
-					_putchar((product / 10) % 10 + '0');
-					_putchar(product % 10 + '0');
+					_putchar(((product / 10) % 10) + '0');
+					_putchar((product % 10) + '0');
 				}
 			}
 			_putchar('\n');
