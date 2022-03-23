b20ca2c1118550b05fe0f4cb12bf9500974cd11b
dgikonyo <denniskimani05@gmail.com>
2022-03-23 12:13:21 +0300

    task 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+	long int count, j, k, next;
+
+	j = 1;
+	k = 2;
+
+	for (count = 1; count <= 98; ++count)
+	{
+		if (count == 98)
+		{
+			printf("%ld\n", j);
+		}
+		else
+		{
+			printf(", %ld", j);
+		}
+		next = j + k;
+		j = k;
+		k = next;
+	}
+	return (0);
+}
