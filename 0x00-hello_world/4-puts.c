1a2a4b70bc7a1238f78de77440f91b47b277a2f4
dgikonyo <denniskimani05.com>
2022-09-08 11:30:32 -0700

    task 4 and 5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Description: Prints "Programming is like building a multilingual puzzle,"
+ * 
+ *Return : Always 0 for success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
