3581ecb90601ae0f2a8a8f78871126786f4884cf
dgikonyo <denniskimani05@gmail.com>
2022-09-14 22:27:25 +0300

    task 8 fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,7 +11,7 @@ void times_table(void)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			k = j * 1;
+			k = j * i;
 			if (j == 0)
 			{
 				_putchar(k + '0');
@@ -20,6 +20,7 @@ void times_table(void)
 			if (k < 10 && j != 0)
 			{
 				_putchar(',');
+				_putchar(' ');
 				_putchar(' ');
 				_putchar(k + '0');
 			} else if (k >= 10)
