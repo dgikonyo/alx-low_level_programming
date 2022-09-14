ae3f8ae28e46420fc6cce420bb331c6a67b35d08
dgikonyo <denniskimani05@gmail.com>
2022-09-14 20:25:58 +0300

    added task 2

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints _putchar, followed by a new line.
+ * Returns: 0 (Always sucessful
+ */
+int main(void)
+{
+        putchar ('_');
+        putchar ('p');
+        putchar ('u');
+        putchar ('t');
+        putchar ('c');
+        putchar ('h');
+        putchar ('a');
+        putchar ('r');
+        putchar ('\n');
+        return (0);
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-0x00-hellow_world
+0x00-hello_world
+0x01-variables_if_else_while
+0x02-functions_nested_loops
