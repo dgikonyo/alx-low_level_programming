8f5820760ac66f67f6325de35e9b5717849c7bd0
dgikonyo <denniskimani05@gmail.com>
2022-09-15 23:46:35 +0300

    fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,9 +11,12 @@ void print_most_numbers(void)
 
 	for (i = 48; i < 58; i++)
 	{
-		if (i != 50 && i != 52)
+		if (i != 50)
 		{
-			_putchar(i);
+			if (i != 52)
+			{
+				_putchar(i);
+			}
 		}
 	}
 }
