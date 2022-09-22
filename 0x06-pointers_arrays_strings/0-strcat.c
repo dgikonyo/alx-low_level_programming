43531794af335cf6da73939ff31abf4736af66d4
dgikonyo <denniskimani05@gmail.com>
2022-09-22 21:39:29 +0300

    tasfk 0 fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,27 +1,23 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * *_strcat - function commute srtings
- * @dest: param pointer to a char
- * @src: param pointer to a char
+ * _strcat - concatenates two strings
+ * @dest: string to append to
+ * @src: string to add
  *
- * Return: return value of dest
+ * Return: a pointer to the resulting string
  */
-
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int i, j;
 
 	i = 0;
 	j = 0;
 
 	while (dest[i] != '\0')
-	{
 		i++;
-	}
 
-	while(src[j] != '\0')
+	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
 		j++;
