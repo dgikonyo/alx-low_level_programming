c44189ac5c1d5bd631b16a2bd0226500374875b8
dgikonyo <denniskimani05@gmail.com>
2022-10-13 23:40:32 +0300

    task 1 fix 3

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -17,7 +17,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 		return;
 	}
 
-	for (index = 0; index <= size; index++)
+	for (index = 0; index < size; index++)
 	{
 		action(array[index]);
 	}
