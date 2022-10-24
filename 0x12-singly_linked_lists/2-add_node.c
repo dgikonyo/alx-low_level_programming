9183eb41f92242f1f74da3dfafe8dd9c23b61c0e
dgikonyo <denniskimani05@gmail.com>
2022-10-24 23:19:35 +0300

    task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,23 +1,41 @@
-#include <stdlib.h>
-#include <string.h>
 #include "lists.h"
 
 /**
- * add_node - adds a new node at the beginning of a linked list
- * @head: double pointer to the list_t list
- * @str: new string to add in the node
- *
- * Return: the address of the new element, or NULL if it fails
+ * _strlen - counts the lenght of a string
+ * @str: string to be counted
+ * Return: len of a string
  */
+
+int _strlen(const char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * add_node - adds a node at the beginning
+ * @head: first parameter
+ * @str: second parameter
+ * Return: nodes
+ */
+
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
 
 	new = malloc(sizeof(list_t));
-	if (new == NULL)
-		return (NULL);
 
-	new->n = n;
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->str = strdup(str);
+	new->len = _strlen(str);
 	new->next = *head;
 
 	*head = new;
